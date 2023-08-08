3a809845f92aef55d385e4c4e6d6a62de402f4b1
Chris Lattman <natgeo13@gmail.com>
2023-08-08 14:27:33 -0400

    cross-platform

--- example.c
+++ example.c
@@ -1,18 +1,30 @@
 // #include <sys/syscall.h> // includes SYS_write
 #include <unistd.h>
 #include <string.h>
+// #include <stdio.h>
+
+#if defined(__APPLE__)
+    #ifndef _SYS_SYSCALL_H_
+        #define SYS_write 4
+    #endif
+#elif defined(__linux__)
+    #ifndef _SYSCALL_H
+        #if defined(__amd64__)
+            #define SYS_write 1
+        #elif defined(__aarch64__)
+            #define SYS_write 64
+        #endif
+    #endif
+#endif
 
 int main(void)
 {
     const char* str = "Hello, world!\n";
     size_t len = strlen(str);
+    // printf("%s", str);
     // write(STDOUT_FILENO, str, len);
 
-#ifdef __linux__
-    syscall(1, STDOUT_FILENO, str, len);
-#elif __APPLE__
-    syscall(4, STDOUT_FILENO, str, len);
-#endif
+    syscall(SYS_write, STDOUT_FILENO, str, len);
 
     return 0;
 }
